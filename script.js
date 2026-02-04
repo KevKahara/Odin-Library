5fc58760272d88fb0c78beb3eec534e1b46036db
Kevin Kahara <kaharakevin254@gmail.com>
2026-02-04 18:50:19 +0300

    Add some bits of styling then go on to manipulate the DOM

---
+++ script.js
@@ -0,0 +1,19 @@
+//Have an array where you will store books
+
+const myLibrary = [];
+
+//Constructor of books with each book will have a unique id
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+//function that will create a book and then add the book to the array
+function addBooktoLibrary() {
+    addBooktoLibrary.push();
+}
+
